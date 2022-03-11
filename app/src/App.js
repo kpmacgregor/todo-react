6efeae5f0d287945f3d0939adf86f66094125f44
K. P. MacGregor <kpsmacgregor@gmail.com>
2022-03-10 18:27:45 -0700

    Uses timestamp for unique id.

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import TodoInput from './TodoInput';
-import TodoList from './TodoList';
-import TodoCompletedList from './TodoCompletedList';
+import React from "react";
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
+import TodoCompletedList from "./TodoCompletedList";
 
 import "./App.css";
 
 class TodoApp extends React.Component {
-  #taskCount = 0;
-
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,34 +13,34 @@ class TodoApp extends React.Component {
     this.state = {
       todoList: [],
       completedList: [],
-    }
+    };
   }
-  
+
   handleSubmit(e) {
     e.preventDefault();
     const task = {
-      id: `task-list-id-${this.#taskCount++}`,
+      id: `task-list-id-${new Date().getTime()}`,
       completed: false,
-      text: e.target.querySelector('input').value,
-    }
+      text: e.target.querySelector("input").value,
+    };
 
     this.setState({ todoList: [...this.state.todoList, task] });
   }
 
   handleChecked(e) {
     // e.preventDefault();
-    let sourceList = 'todoList',
-        destinationList = 'completedList';
+    let sourceList = "todoList",
+      destinationList = "completedList";
     if (!e.target.checked) {
-      sourceList = 'completedList';
-      destinationList = 'todoList';
+      sourceList = "completedList";
+      destinationList = "todoList";
     }
-    const index = this.state[sourceList].findIndex(task => {
-      return task.id === e.target.id
+    const index = this.state[sourceList].findIndex((task) => {
+      return task.id === e.target.id;
     });
     const task = this.state[sourceList][index];
     task.completed = !task.completed;
-    
+
     const _sourceList = [...this.state[sourceList]];
     _sourceList.splice(index, 1);
     const _destinationList = [...this.state[destinationList], task];
@@ -56,23 +54,24 @@ class TodoApp extends React.Component {
     // this.setState({ destinationList: _destinationList });
   }
 
-  handleUnchecked(e) {
-
-  }
+  handleUnchecked(e) {}
 
   render() {
-   return (
+    return (
       <div className="App">
         <header className="App-header">
           <h1>tasks</h1>
         </header>
         <TodoInput onSubmit={this.handleSubmit} />
-       <TodoList todoList={this.state.todoList} onChecked={this.handleChecked} />
-       <hr />
-       {/* <TodoCompletedList completedList={this.state.completedList} /> */}
+        <TodoList
+          todoList={this.state.todoList}
+          onChecked={this.handleChecked}
+        />
+        <hr />
+        {/* <TodoCompletedList completedList={this.state.completedList} /> */}
       </div>
-    )
-  };
+    );
+  }
 }
 
 export default TodoApp;
